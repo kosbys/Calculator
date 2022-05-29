d0e8bff0a6853d48e0dd78cfadb898a08516bcc5
kosbys <kosmicabysslord@protonmail.com>
2022-05-29 17:23:23 +0300

    Add nums array

--- script.js
+++ script.js
@@ -25,12 +25,39 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  return roundPrecised(a / b, 2);
 }
 
 function square(a) {
-    return a * a;
+  return a * a;
 }
 
-container = document.querySelector(`.container`)
-digits = document.querySelectorAll(`.digit`)
+function roundPrecised(number, precision) {
+  let power = Math.pow(10, precision);
+
+  return Math.round(number * power) / power;
+}
+
+container = document.querySelector(`.container`);
+digits = document.querySelectorAll(`.digit`);
+display = document.querySelector(`.display`);
+
+nums = [];
+
+let x = [...digits].map((digit) => {
+  digit.addEventListener("click", () => {
+    if (nums.length == 0) {
+      nums.push(digit.value);
+      display.innerText = digit.value;
+    } else {
+        nums.push(digit.value);
+        display.innerText = nums.reduce((total, num, i) => {
+            /* reduce the sum of the array to be each digit times 10^i */
+            console.log(`index ${i}`);
+            console.log(`number ${num}`);
+
+            return num * i;
+        });
+    }
+  });
+});
